73997d825ce8760a9cb702366187615f1235f799
arman-2359media <arman.rozika@2359media.com>
2019-10-11 22:17:01 +0700

    add my own json, flip json is cors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 import './App.css';
 
 function App() {
+  const [state, setState] = useState({
+    list: []
+  });
+
+  useEffect(() => {
+    // fetch('https://nextar.flip.id/frontend-test', {
+    //   method: 'GET',
+    //   headers: {
+    //     Accept: 'application/json',
+    //     'Content-Type': 'application/json'
+    //   }
+    // })
+    //   .then(res => {
+    //     return res;
+    //   })
+    //   .then(item => {
+    //     console.log(item);
+    //   });
+  }, [state]);
+
   return (
     <div className="App">
-      <p>sss</p>
-      <p>sss</p>
+      <div className="input-wrapper">
+        <FontAwesomeIcon className="faicon" icon={faSearch} />
+        <input type="text" placeholder="Cari nama" />
+      </div>
+
+      <div className="client-list">
+        <div className="green-line"></div>
+        <div className="list-content">
+          <div className="content-left">
+            <h3>BNI BRI</h3>
+            <p>SALAMAH</p>
+            <p>Rp 250000 &#9679; 5 Agustus 2019</p>
+          </div>
+          <button className="btn-status">Berhasil</button>
+        </div>
+      </div>
     </div>
   );
 }
